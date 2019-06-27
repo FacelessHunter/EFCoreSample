e28bf23829c2d43733741099fe2ed7f34d37fe74
Roman Tikhiy <roman.tykhyi@magnise.com>
2019-06-27 18:52:30 +0300

    Implemented basic structure of ef core sample project

---
+++ EfCoreSample/EfCoreSample.Persistance/EfCoreSampleDbContext.cs
@@ -0,0 +1,21 @@
+﻿using EfCoreSample.Persistance.EntityConfiguration;
+using Microsoft.EntityFrameworkCore;
+
+namespace EfCoreSample.Persistance
+{
+    public class EfCoreSampleDbContext : DbContext
+    {
+        public const string SchemaName = "efcoresample";
+
+        public EfCoreSampleDbContext(DbContextOptions<EfCoreSampleDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfiguration(new AddressEntityConfiguration());
+            builder.ApplyConfiguration(new EmployeeDepartmentEntityConfiguration());
+            builder.ApplyConfiguration(new EmployeeEntityConfiguration());
+        }
+    }
+}
