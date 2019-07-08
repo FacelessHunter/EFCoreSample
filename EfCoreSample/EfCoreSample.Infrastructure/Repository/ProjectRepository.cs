7c9cbd349f54f24a38376cd9194b7bb1584f54c5
FacelessHunter <zoro.130898@gmail.com>
2019-07-08 09:47:02 +0300

    Added Repositiry and Controler

---
+++ EfCoreSample/EfCoreSample.Infrastructure/Repository/ProjectRepository.cs
@@ -0,0 +1,62 @@
+﻿using EfCoreSample.Doman;
+using EfCoreSample.Infrastructure.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EfCoreSample.Persistance;
+using Microsoft.EntityFrameworkCore;
+using EfCoreSample.Infrastructure;
+namespace EfCoreSample.Infrastructure
+{
+    public class ProjectRepository : IRepository<Project, long>
+    {
+        private EfCoreSampleDbContext db { get; set;}
+        public ProjectRepository(EfCoreSampleDbContext context)
+        {
+            db = context;
+        }
+
+        public async Task<Project> FindAsync(long key)
+        {
+            
+            return await db.Projects.FirstOrDefaultAsync(p => p.Id == key);
+        }
+
+        public async Task<IEnumerable<Project>> GetAsync(Expression<Func<Project, bool>> expression)
+        {
+
+            return null; //await db.Projects.<IEnumerable<Project>>(expression);
+        }
+
+        public Task<Project> InsertAsync(Project item)
+        {
+            return null;
+        }
+        public Task<bool> IsExistAsync(long key)
+        {
+            return null;
+        }
+        public void UpdateRange(IEnumerable<Project> items)
+        {
+            
+        }
+        public Project Update(Project item)
+        {
+            return null;
+        }
+        public bool Remove(Project item)
+        {
+            return false;
+        }
+        public bool Remove(long key)
+        {
+            return false;
+        }
+
+
+
+    }
+}
---
+++ EfCoreSample/EfCoreSample/Controllers/DefaultController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EfCoreSample.Doman;
+using EfCoreSample.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace EfCoreSample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        private IService Service { get; set; }
+        public DefaultController(IService service)
+        {
+            Service = service;
+        }
+        [Route("{i}")]
+        public async Task<Project> Get(int i)
+        {
+            
+            return await Service.Get(i);
+        }
+    }
+}
